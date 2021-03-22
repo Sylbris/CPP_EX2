d49365e5e52c7cb9bf73162ea0b33f18c78dc3b6
Sylbris <dan.davydov1@gmail.com>
2021-03-22 19:08:15 +0200

    Added tests

--- Test.cpp
+++ Test.cpp
@@ -37,12 +37,12 @@ TEST_CASE("Good post") {
     'r','s','t','u','v','w','x',
     'y','z'};
     string ran = "";
-    uint n = (uint)rand();
-    for (int i=0;i<n;i++) {
+    //uint n = (uint)rand() % row;
+    for (int i=0;i<10;i++) {
         ran=ran + letters[rand() % 26];
     }
     board.post(/*row=*/row, /*column=*/col, Direction::Horizontal, ran);
-    CHECK(board.read(/*row=*/row, /*column=*/col, Direction::Horizontal, /*length=*/n) == ran);
+    CHECK(board.read(/*row=*/row, /*column=*/col, Direction::Horizontal, /*length=*/10) == ran);
 
     //Post specific word
     board.post(/*row=*/100, /*column=*/200, Direction::Horizontal, "abcd");
@@ -68,6 +68,13 @@ TEST_CASE("Good post") {
     board.post(/*row=*/100, /*column=*/200, Direction::Horizontal, "____");
     CHECK(board.read(/*row=*/99, /*column=*/201, Direction::Vertical, /*length=*/3) == "___");
     CHECK(board.read(/*row=*/100, /*column=*/200, Direction::Horizontal, /*length=*/4) == "____");
+    board.post(/*row=*/101, /*column=*/200, Direction::Vertical, "oh");
+    CHECK(board.read(/*row=*/100, /*column=*/200, Direction::Vertical, /*length=*/3) == "_oh");
+    CHECK(board.read(/*row=*/100, /*column=*/200, Direction::Horizontal, /*length=*/4) == "____");
+    board.post(/*row=*/105, /*column=*/200, Direction::Vertical, "ho");
+    CHECK(board.read(/*row=*/106, /*column=*/200, Direction::Vertical, /*length=*/3) == "_ho");
+    CHECK(board.read(/*row=*/106, /*column=*/200, Direction::Horizontal, /*length=*/3) == "_ho");
+
     //Border
     board.post(/*row=*/0, /*column=*/0, Direction::Horizontal, "rps");
     CHECK(board.read(/*row=*/0, /*column=*/0, Direction::Vertical, /*length=*/3) == "r__");
